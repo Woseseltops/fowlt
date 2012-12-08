e4061dc77fa581d179cf9d778dd863a9fe81194b
Antal van den Bosch <a.vandenbosch@let.ru.nl>
2012-12-08 21:00:26 +0100

    keep LD at default 1, not 3

--- servers/start_woprserver.py
+++ servers/start_woprserver.py
@@ -7,10 +7,10 @@ print('Starting up server');
 
 try:
     if settings['wopr_large_corpus'] == '1':
-        serverhelp.command(settings['wopr_location']+' -r server_sc -p ibasefile:woprserver/BNC.ibase,timbl:"-a1 +D",lexicon:woprserver/BNC.lex,port:3001,keep:1,mwl:5,mld:3,max_distr:250,min_ratio:100 &');
+        serverhelp.command(settings['wopr_location']+' -r server_sc -p ibasefile:woprserver/BNC.ibase,timbl:"-a1 +D",lexicon:woprserver/BNC.lex,port:3001,keep:1,mwl:5,max_distr:250,min_ratio:100 &');
 
     elif settings['wopr_large_corpus'] == '0':
-        serverhelp.command(settings['wopr_location']+' -r server_sc -p ibasefile:woprserver/BNC_small.ibase,timbl:"-a1 +D",lexicon:woprserver/BNC_small.lex,port:3001,keep:1,mwl:5,mld:3,max_distr:250,min_ratio:100 &');
+        serverhelp.command(settings['wopr_location']+' -r server_sc -p ibasefile:woprserver/BNC_small.ibase,timbl:"-a1 +D",lexicon:woprserver/BNC_small.lex,port:3001,keep:1,mwl:5,max_distr:250,min_ratio:100 &');
 
 except AttributeError:
     print('An error occured. Make sure all your settings are correct.');
