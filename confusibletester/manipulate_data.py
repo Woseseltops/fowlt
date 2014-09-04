f7b3a220707bedee1b6461d04e8eb8b2f32b42b3
Wessel Stoop <wstoop@applejack.science.ru.nl>
2014-09-04 16:43:33 +0200

    Made data manipulation script easier to reuse

--- confusibletester/manipulate_data.py
+++ confusibletester/manipulate_data.py
@@ -44,81 +44,83 @@ def get_classes(inputfile):
 # Script starts here
 #=================================
 
-#Parameters
-classes = ['than','then'];
-testtrain_ratio = 0.1;
-trainerror_ratio = 0.1;
-testerror_ratio = 0.1;
+if __name__ == '__main__':
 
-inputfile = 'src_data/'+','.join(classes)+'.inst';
-outputfile_prefix = 'manipulated_data/'+','.join(classes);
+	#Parameters
+	classes = ['than','then'];
+	testtrain_ratio = 0.1;
+	trainerror_ratio = 0.1;
+	testerror_ratio = 0.1;
 
-#Create the various outputfiles
-output = {};
+	inputfile = 'src_data/'+','.join(classes)+'.inst';
+	outputfile_prefix = 'manipulated_data/'+','.join(classes);
 
-for kind in ['noclass','withclass','err']:
+	#Create the various outputfiles
+	output = {};
+
+	for kind in ['noclass','withclass','err']:
 	
-	output[kind] = {};
+		output[kind] = {};
 
-	for use in ['test','train']:
-		output[kind][use] = open(outputfile_prefix+'.'+kind+'.'+use+'.inst','w');
+		for use in ['test','train']:
+			output[kind][use] = open(outputfile_prefix+'.'+kind+'.'+use+'.inst','w');
 
-#Go through the input file and rewrite it
-testline = 1/testtrain_ratio;
-trainerrorline = 1/trainerror_ratio;
-testerrorline = 1/testerror_ratio;
+	#Go through the input file and rewrite it
+	testline = 1/testtrain_ratio;
+	trainerrorline = 1/trainerror_ratio;
+	testerrorline = 1/testerror_ratio;
 
-n_train = -1
-n_test = -1;
+	n_train = -1
+	n_test = -1;
 
-for n, line in enumerate(open(inputfile)):
+	for n, line in enumerate(open(inputfile)):
 	
-	#Figure out what to use this line for (train or test),
-	#and whether to put in a fake error
-	insert_error = False;
+		#Figure out what to use this line for (train or test),
+		#and whether to put in a fake error
+		insert_error = False;
 	
-	if n%testline == 0:
-		use = 'test';
-		n_test += 1;
+		if n%testline == 0:
+			use = 'test';
+			n_test += 1;
 		
-		if n_test%testerrorline == 0:
-			insert_error = True;
+			if n_test%testerrorline == 0:
+				insert_error = True;
 
-	else:
-		use = 'train';
-		n_train += 1;
+		else:
+			use = 'train';
+			n_train += 1;
 
-		if n_train%trainerrorline == 0:
-			insert_error = True;
+			if n_train%trainerrorline == 0:
+				insert_error = True;
 
-	#String -> instance
-	inst = interpret_string(line,classes);
+		#String -> instance
+		inst = interpret_string(line,classes);
 	
-	#Reformat
-	lcontext = ' '.join(inst.leftcontext);
-	rcontext = ' '.join(inst.rightcontext);
+		#Reformat
+		lcontext = ' '.join(inst.leftcontext);
+		rcontext = ' '.join(inst.rightcontext);
 
-	if use == 'train': #For training, the right class is always at the end
-		line_noclass = [lcontext,rcontext,inst.cl];		
-		line_withclass = [lcontext,inst.cl,rcontext,inst.cl];
+		if use == 'train': #For training, the right class is always at the end
+			line_noclass = [lcontext,rcontext,inst.cl];		
+			line_withclass = [lcontext,inst.cl,rcontext,inst.cl];
 
-		if insert_error: #But there can be a wrong feature
-			line_err = [lcontext,inst.pick_other_class(),rcontext,inst.cl];
-		else:
-			line_err = [lcontext,inst.cl,rcontext,inst.cl];
+			if insert_error: #But there can be a wrong feature
+				line_err = [lcontext,inst.pick_other_class(),rcontext,inst.cl];
+			else:
+				line_err = [lcontext,inst.cl,rcontext,inst.cl];
 
-	elif use == 'test': #For testing, you don't know, so you pick what was in the
-				        #material
-		if insert_error:
-			cl = inst.pick_other_class();
-		else:
-			cl = inst.cl;
+		elif use == 'test': #For testing, you don't know, so you pick what was in the
+						    #material
+			if insert_error:
+				cl = inst.pick_other_class();
+			else:
+				cl = inst.cl;
 
-		line_noclass = [lcontext,rcontext,cl];		
-		line_withclass = [lcontext,cl,rcontext,cl];
-		line_err = [lcontext,cl,rcontext,cl];
+			line_noclass = [lcontext,rcontext,cl];		
+			line_withclass = [lcontext,cl,rcontext,cl];
+			line_err = [lcontext,cl,rcontext,cl];
 
-	#Output
-	output['noclass'][use].write(' '.join(line_noclass)+'\n');
-	output['withclass'][use].write(' '.join(line_withclass)+'\n');
-	output['err'][use].write(' '.join(line_err)+'\n');
+		#Output
+		output['noclass'][use].write(' '.join(line_noclass)+'\n');
+		output['withclass'][use].write(' '.join(line_withclass)+'\n');
+		output['err'][use].write(' '.join(line_err)+'\n');
